894836801442a70bd42cd563050a11edce036808
pom <113845723+pompompur1nn@users.noreply.github.com>
2023-08-05 00:26:56 -0400

    Add files via upload

---
+++ js/page/roprolegalteamreadthisinatexteditor.js
@@ -0,0 +1,14 @@
+/*
+
+RoPro (https://ropro.io) v1.5
+
+The RoPro extension is developed by:
+                               
+,------.  ,--. ,-----.,------. 
+|  .-.  \ |  |'  .--./|  .---' 
+|  |  \  :|  ||  |    |  `--,  
+|  '--'  /|  |'  '--'\|  `---. 
+`-------' `--' `-----'`------' 
+                            
+I don't make any money off of this, and it is open src. When you guys get your act together and release the Firefox build (which this proves you can do) I will gladly take it down!
+*/
